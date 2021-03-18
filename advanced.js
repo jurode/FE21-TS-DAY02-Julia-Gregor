102c862185d95e291347e6cc2ed9acec919c2a0a
Julia Demel <julia.demel@gmail.com>
2021-03-18 16:02:50 +0100

    add html-creation + ts

--- advanced.js
+++ advanced.js
@@ -19,47 +19,6 @@ var __extends = (this && this.__extends) || (function () {
 //     Consider the inheritance concept
 //     Define 2 other Child classes and name them Motorbikes and Trucks
 //     Based on the personal vehicle performance model, kilometers left, number of seats, fuel type and year of production calculate the price once the user clicks on the button "show price" *use your dummy custom formula)
-// var cars : Array<{
-//     brand: string;
-//     model: string;
-//     wheels: number;
-//     doors: number;
-//     seats: number;
-//     fuelType: string;
-//     kilometers: number;
-//     productionYear: number;
-//     automaticGear: boolean;
-//     weight: number;
-//     color: string;
-// }> = [];
-// var trucks : Array<{
-//     brand: string;
-//     model: string;
-//     wheels: number;
-//     doors: number;
-//     seats: number;
-//     fuelType: string;
-//     kilometers: number;
-//     productionYear: number;
-//     automaticGear: boolean;
-//     weight: number;
-//     color: string;
-//     transportWeight: number;
-// }> = [];
-// var bikes : Array<{
-//     brand: string;
-//     model: string;
-//     wheels: number;
-//     doors: number;
-//     seats: number;
-//     fuelType: string;
-//     kilometers: number;
-//     productionYear: number;
-//     automaticGear: boolean;
-//     weight: number;
-//     color: string;
-//     maxSpeed: number;
-// }> = [];
 var Vehicles = /** @class */ (function () {
     function Vehicles(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color) {
         this.brand = brand;
@@ -98,7 +57,92 @@ var Motorbikes = /** @class */ (function (_super) {
     }
     return Motorbikes;
 }(Vehicles));
-// import data from JSON
-var vehicles = JSON.parse(cars);
-var abfc = JSON.parse(trucks);
-var werafe = JSON.parse(bikes);
+// == import data from JSON
+var carsArr = JSON.parse(cars);
+var trucksArr = JSON.parse(trucks);
+var bikesArr = JSON.parse(bikes);
+for (var i = 0; i < carsArr.length; i++) {
+    // container for each object (truck, motor...)
+    var objContainer = document.createElement("div");
+    objContainer.classList.add("card");
+    objContainer.setAttribute("style", "width: 18rem;");
+    document.getElementById("v-pills-all").appendChild(objContainer);
+    var objImg = document.createElement("img");
+    objImg.classList.add("card-img-top");
+    objImg.setAttribute("src", "https://www.flaticon.com/svg/vstatic/svg/3202/3202926.svg?token=exp=1616072910~hmac=041b47e59a7828df61c512f91c79c374");
+    objContainer.appendChild(objImg);
+    var objBody = document.createElement("div");
+    objBody.classList.add("card-body");
+    objContainer.appendChild(objBody);
+    var objTitle = document.createElement("h5");
+    objTitle.classList.add("card-title");
+    var objTitleText = document.createTextNode(carsArr[i].brand + " " + carsArr[i].model);
+    objTitle.appendChild(objTitleText);
+    objBody.appendChild(objTitle);
+    var objAttributes = document.createElement("p");
+    objAttributes.classList.add("card-text", "my-0");
+    var objAttributesText = document.createTextNode("Wheels: " + carsArr[i].wheels);
+    objAttributes.appendChild(objAttributesText);
+    objBody.appendChild(objAttributes);
+    var objAttributes = document.createElement("p");
+    objAttributes.classList.add("card-text", "my-0");
+    var objAttributesText = document.createTextNode("Fuel: " + carsArr[i].fuelType);
+    objAttributes.appendChild(objAttributesText);
+    objBody.appendChild(objAttributes);
+    var objAttributes = document.createElement("p");
+    objAttributes.classList.add("card-text", "my-0");
+    var objAttributesText = document.createTextNode("Doors: " + carsArr[i].doors);
+    objAttributes.appendChild(objAttributesText);
+    objBody.appendChild(objAttributes);
+    var objAttributes = document.createElement("p");
+    objAttributes.classList.add("card-text", "my-0");
+    var objAttributesText = document.createTextNode("Kilometers: " + carsArr[i].kilometers);
+    objAttributes.appendChild(objAttributesText);
+    objBody.appendChild(objAttributes);
+    var objButton = document.createElement("button");
+    objButton.classList.add("calcPriceBtn", "btn", "btn-primary", "mt-2");
+    var objButtonText = document.createTextNode("Price");
+    objButton.appendChild(objButtonText);
+    objBody.appendChild(objButton);
+}
+var priceBtns = Array.from(document.getElementsByClassName("calcPriceBtn"));
+console.log(priceBtns);
+var _loop_1 = function (i) {
+    priceBtns[i].addEventListener("click", function () {
+        Swal.fire('Price: € ' + calcPrice());
+    });
+    function calcPrice() {
+        var fuelType;
+        if (carsArr[i].fuelType = "electric") {
+            fuelType = 1000;
+        }
+        else if (carsArr[i].fuelType = "gasoline") {
+            fuelType = 600;
+        }
+        else if (carsArr[i].fuelType = "diesel") {
+            fuelType = 700;
+        }
+        ;
+        var calcKilometers;
+        if (carsArr[i].kilometers > 300000) {
+            calcKilometers = -200;
+        }
+        else if (carsArr[i].kilometers > 200000) {
+            calcKilometers = -100;
+        }
+        else if (carsArr[i].kilometers > 100000) {
+            calcKilometers = 100;
+        }
+        else if (carsArr[i].kilometers > 50000) {
+            calcKilometers = 250;
+        }
+        else if (carsArr[i].kilometers > 20000) {
+            calcKilometers = 1000;
+        }
+        var objCalcPrice = carsArr[i].wheels * 300 + carsArr[i].doors * 400 + carsArr[i].seats * 300 + calcKilometers + fuelType;
+        return objCalcPrice;
+    }
+};
+for (var i = 0; i < priceBtns.length; i++) {
+    _loop_1(i);
+}
--- advanced.ts
+++ advanced.ts
@@ -6,49 +6,6 @@
 //     Define 2 other Child classes and name them Motorbikes and Trucks
 //     Based on the personal vehicle performance model, kilometers left, number of seats, fuel type and year of production calculate the price once the user clicks on the button "show price" *use your dummy custom formula)
 
-// var cars : Array<{
-//     brand: string;
-//     model: string;
-//     wheels: number;
-//     doors: number;
-//     seats: number;
-//     fuelType: string;
-//     kilometers: number;
-//     productionYear: number;
-//     automaticGear: boolean;
-//     weight: number;
-//     color: string;
-// }> = [];
-
-// var trucks : Array<{
-//     brand: string;
-//     model: string;
-//     wheels: number;
-//     doors: number;
-//     seats: number;
-//     fuelType: string;
-//     kilometers: number;
-//     productionYear: number;
-//     automaticGear: boolean;
-//     weight: number;
-//     color: string;
-//     transportWeight: number;
-// }> = [];
-
-// var bikes : Array<{
-//     brand: string;
-//     model: string;
-//     wheels: number;
-//     doors: number;
-//     seats: number;
-//     fuelType: string;
-//     kilometers: number;
-//     productionYear: number;
-//     automaticGear: boolean;
-//     weight: number;
-//     color: string;
-//     maxSpeed: number;
-// }> = [];
 class Vehicles {
     brand: string;
     model: string;
@@ -61,7 +18,7 @@ class Vehicles {
     automaticGear: boolean;
     weight: number;
     color: string;
-    constructor(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color){
+    constructor(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color) {
         this.brand = brand;
         this.model = model;
         this.wheels = wheels;
@@ -75,7 +32,7 @@ class Vehicles {
         this.color = color;
     }
 
-    calcPrice(){
+    calcPrice() {
         let price = 50000 - (this.kilometers * 0.1);
         return price;
     }
@@ -83,24 +40,119 @@ class Vehicles {
 
 class Trucks extends Vehicles {
     transportWeight: number;
-    constructor(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color, transportWeight){
+    constructor(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color, transportWeight) {
         super(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color);
         this.transportWeight = transportWeight;
     }
 }
 
-class Motorbikes extends Vehicles{
+class Motorbikes extends Vehicles {
     maxSpeed: number;
-    constructor(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color, maxSpeed){
+    constructor(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color, maxSpeed) {
         super(brand, model, wheels, doors, seats, fuelType, kilometers, productionYear, automaticGear, weight, color);
         this.maxSpeed = maxSpeed;
     }
 }
 
-// import data from JSON
-let vehicles = JSON.parse(cars);
-let abfc = JSON.parse(trucks);
-let werafe : HTMLElement = JSON.parse(bikes);
+
+// == import data from JSON
+
+let carsArr = JSON.parse(cars);
+let trucksArr = JSON.parse(trucks);
+let bikesArr = JSON.parse(bikes);
+
+for (let i = 0; i < carsArr.length; i++) {
+
+    // container for each object (truck, motor...)
+    var objContainer = document.createElement("div"); 
+    objContainer.classList.add("card");
+    objContainer.setAttribute("style", "width: 18rem;");
+    document.getElementById("v-pills-all").appendChild(objContainer);
+
+    var objImg = document.createElement("img");
+    objImg.classList.add("card-img-top");
+    objImg.setAttribute("src", "https://www.flaticon.com/svg/vstatic/svg/3202/3202926.svg?token=exp=1616072910~hmac=041b47e59a7828df61c512f91c79c374");
+    objContainer.appendChild(objImg);
+
+    var objBody = document.createElement("div");
+    objBody.classList.add("card-body");
+    objContainer.appendChild(objBody);
+
+    var objTitle = document.createElement("h5");
+    objTitle.classList.add("card-title");
+    var objTitleText = document.createTextNode(carsArr[i].brand + " " + carsArr[i].model);
+    objTitle.appendChild(objTitleText);
+    objBody.appendChild(objTitle);
+
+    var objAttributes = document.createElement("p");
+    objAttributes.classList.add("card-text", "my-0");
+    var objAttributesText = document.createTextNode("Wheels: " + carsArr[i].wheels);
+    objAttributes.appendChild(objAttributesText);
+    objBody.appendChild(objAttributes);
+
+    var objAttributes = document.createElement("p");
+    objAttributes.classList.add("card-text", "my-0");
+    var objAttributesText = document.createTextNode("Fuel: " + carsArr[i].fuelType);
+    objAttributes.appendChild(objAttributesText);
+    objBody.appendChild(objAttributes);
+
+    var objAttributes = document.createElement("p");
+    objAttributes.classList.add("card-text", "my-0");
+    var objAttributesText = document.createTextNode("Doors: " + carsArr[i].doors);
+    objAttributes.appendChild(objAttributesText);
+    objBody.appendChild(objAttributes);
+
+    var objAttributes = document.createElement("p");
+    objAttributes.classList.add("card-text", "my-0");
+    var objAttributesText = document.createTextNode("Kilometers: " + carsArr[i].kilometers);
+    objAttributes.appendChild(objAttributesText);
+    objBody.appendChild(objAttributes);
+
+    var objButton = document.createElement("button");
+    objButton.classList.add("calcPriceBtn", "btn", "btn-primary", "mt-2");
+    var objButtonText = document.createTextNode("Price");
+    objButton.appendChild(objButtonText);
+    objBody.appendChild(objButton);
+
+}
+
+let priceBtns = Array.from(document.getElementsByClassName("calcPriceBtn"));
+console.log(priceBtns);
 
 
+for (let i = 0; i < priceBtns.length; i++){
+    priceBtns[i].addEventListener("click", function(){
+        Swal.fire('Price: € ' + calcPrice());
+        
+    });
+    
+    function calcPrice(){
+        
+        let fuelType : number;
+        if (carsArr[i].fuelType = "electric"){
+            fuelType = 1000;
+        } else if (carsArr[i].fuelType = "gasoline"){
+            fuelType = 600;
+        } else if (carsArr[i].fuelType = "diesel"){
+            fuelType = 700;
+        };
+
+        let calcKilometers : number;
+        if (carsArr[i].kilometers > 300000) {
+            calcKilometers = -200;
+        } else if (carsArr[i].kilometers > 200000) {
+            calcKilometers = -100;
+        } else if (carsArr[i].kilometers > 100000) {
+            calcKilometers = 100;
+        } else if (carsArr[i].kilometers > 50000) {
+            calcKilometers = 250;
+        } else if (carsArr[i].kilometers > 20000) {
+            calcKilometers = 1000;
+        }
+    
+        let objCalcPrice : number = carsArr[i].wheels * 300 + carsArr[i].doors * 400 + carsArr[i].seats * 300 + calcKilometers + fuelType;
+        return objCalcPrice;
+    }
+    
+}
 
